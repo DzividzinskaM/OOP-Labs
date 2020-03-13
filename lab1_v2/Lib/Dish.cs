bb40f72b31ba93baf80502c50bf75d362e01e667
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-03-13 21:58:43 +0200

    lab1 create menu

---
+++ lab1_v2/Lib/Dish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib
+{
+    public class Dish
+    {
+        private string DishName;
+        private double DishPrice;
+
+        public Dish(string n, double p)
+        {
+            DishName = n;
+            DishPrice = p;
+        }
+
+        public string GetDishName()
+        {
+            return DishName + " " + DishPrice.ToString(); 
+        }
+        
+    }
+}
---
+++ lab1_v2/lab1_v2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Lib;
+
+namespace lab1_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************BREAKFAST************");
+            Order order = new Order("breakfast");
+            ShowMenu(order);
+
+            Console.WriteLine();
+
+            Console.WriteLine("************DINNER************");
+            order = new Order("dinner");
+            ShowMenu(order);
+
+            Console.WriteLine();
+
+            Console.WriteLine("************SUPPER************");
+            order = new Order("supper");
+            ShowMenu(order);
+
+
+
+        }
+
+        static void ShowMenu(Order order)
+        {
+            int count = 0;
+
+            foreach(var value in order.GetMenu())
+            {
+                count++;
+                Console.WriteLine($"{count}. {value.GetDishName()}");
+            }
+        }
+    }
+}
