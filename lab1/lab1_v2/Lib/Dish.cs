4e8a32a8c41760d5c80f1f2a0cf53c30acaaaec6
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-03-16 15:08:27 +0200

    Lab1

---
+++ lab1/lab1_v2/Lib/Dish.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib
+{
+    public class Dish
+    {
+        private string DishName;
+        private double DishPrice;
+
+        public Dish(string n, double p)
+        {
+            DishName = n;
+            DishPrice = p;
+        }
+
+        public string GetDishName()
+        {
+            return DishName + " " + DishPrice.ToString(); 
+        }
+
+        public double GetPrice()
+        {
+            return DishPrice;
+        }
+        
+    }
+}
---
+++ lab1/lab1_v2/Lib/Order.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lib
+{
+    public enum MealType 
+    {
+        breakfast, 
+        dinner, 
+        supper
+    }
+    public class Order
+    {
+        private List<Dish> DishesMenu = new List<Dish>();
+        private string Type;
+        private List<string> Bill = new List<string>();
+        private double TotalPrice = 0;
+        public Order(string Mtype)
+        {
+            if(CheckType(Mtype) == true)
+            {
+                CreateMenu();
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+
+        private bool CheckType(string Mtype)
+        {
+            if(Mtype == MealType.breakfast.ToString() 
+                || Mtype == MealType.dinner.ToString()
+                || Mtype == MealType.supper.ToString())
+            {
+                Type = Mtype;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string ShowError()
+        {
+            string Error = "Your type of meal isn't correct";
+            return Error;
+        }
+
+        private void CreateMenu()
+        {
+            if(Type == MealType.breakfast.ToString())
+            {
+                Breakfast breakfast = new Breakfast(ref DishesMenu);
+            }
+            else if(Type == MealType.dinner.ToString())
+            {
+                Dinner dinner = new Dinner(ref DishesMenu);
+            }
+            else
+            {
+                Supper supper = new Supper(ref DishesMenu);
+            }
+        }
+
+        public List<Dish> GetMenu()
+        {
+            return DishesMenu;
+        }
+
+
+        public void AddToBill(int NumberDish)
+        {
+            Dish dish = DishesMenu[NumberDish];
+            Bill.Add(dish.GetDishName());
+            TotalPrice += dish.GetPrice();
+        }
+
+        public List<string> GetBill()
+        {
+            Bill.Add("TOTAL PRICE: " + TotalPrice);
+            return Bill;
+        }
+    }
+}
