ef94f8ace315db0b3d6ee60e52a5ead62fb290ac
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-03-16 14:46:06 +0200

    Add methods for creating bill

---
+++ lab1_v2/Lib/Supper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib
+{
+    internal class Supper : Meal
+    {
+        internal Supper(ref List<Dish> Menu)
+        {
+            SetRandomMenu(ref Menu);
+        }
+        internal override void SetRandomMenu(ref List<Dish> Menu)
+        {
+            Dish dish = new Dish("Chopped Kale Salad", 25);
+            Menu.Add(dish);
+            dish = new Dish("Avocado Salad", 22);
+            Menu.Add(dish);
+            dish = new Dish("Filet of Sole Oreganata", 35);
+            Menu.Add(dish);
+            dish = new Dish("Grilled Skirt Steak", 40);
+            Menu.Add(dish);
+            dish = new Dish("Gnocchi w/ Pesto", 36);
+            Menu.Add(dish);
+            dish = new Dish("Black/green tea", 18);
+            Menu.Add(dish);
+        }
+    }
+}
